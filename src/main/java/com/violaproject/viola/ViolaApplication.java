568bca9569a0d3bb6a9384adb61f29bd01e4ff38
Lina Rusakova <xlinaris@gmail.com>
2021-11-09 16:50:34 +0300

    Initial commit

---
+++ src/main/java/com/violaproject/viola/ViolaApplication.java
@@ -0,0 +1,13 @@
+package com.violaproject.viola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/violaproject/viola/ViolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.violaproject.viola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
